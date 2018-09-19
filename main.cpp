1a5e0003f83a6a2fe63800cb57e1ea42c7f08dcc
drewcornatzer <acornatzer@mail.csuchico.edu>
2018-09-19 16:23:15 -0700

    Asks for first and last name

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors:Drew Cornatzer
 #include<iostream>
 #include<string>
 
@@ -7,9 +7,15 @@ using namespace std;
 int main()
 {
   //declare variables
-
+    string firstname;
+    string lastname;
   //get user input
-
+    cout << "What is your first name?";
+    cout<<endl;
+    cin >> firstname;
+    cout << "What is your last name?";
+    cout<<endl;
+    cin >> lastname;
   //tell fortune
 
   return 0;
